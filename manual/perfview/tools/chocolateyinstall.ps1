e31e2a90fea313e33c5fb8e0eaea97e2234b38e7
ilabutin <ilabutin@gmail.com>
2019-03-04 00:59:44 +0300

    Add package for PerfView

---
+++ manual/perfview/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = 'PerfView'
+$url = 'https://github.com/Microsoft/perfview/releases/download/P2.0.26/PerfView.exe'
+$checksum ='27C67498F01C69C52415C4A35413508A9DB7890247F1993D27DE40C82FE324FC'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile -PackageName PerfView -FileFullPath "$toolsDir\PerfView.exe" -url $url -checksum $checksum -checksumType 'sha256'
