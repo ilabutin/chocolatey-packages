20b6e1e1227ef09c2dccd47d44a3606e7d1e7b27
ilabutin <ilabutin@gmail.com>
2019-05-13 18:09:45 +0300

    Fixed link to PerfView v2.0.42

--- manual/perfview/tools/chocolateyinstall.ps1
+++ manual/perfview/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name = 'PerfView'
-$url = 'https://github.com/microsoft/perfview/releases/download/P2.0.42/PerfView64.exe'
+$url = 'https://github.com/microsoft/perfview/releases/download/P2.0.42/PerfView.exe'
 $checksum ='B5D8EBE0D36CBF039AE99071BA067E2991FE356AF3BC61A13F3012D90D54DF2D'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
