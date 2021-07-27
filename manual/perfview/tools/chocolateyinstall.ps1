ab506a93d5ca4043081fdacdf69eaede42a880ab
Igor Labutin <ilabutin@gmail.com>
2021-07-27 10:07:00 +0300

    Update PerfView 2.0.71

--- manual/perfview/tools/chocolateyinstall.ps1
+++ manual/perfview/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$name = 'PerfView'
-$url = 'https://github.com/microsoft/perfview/releases/download/P2.0.70/PerfView.exe'
-$checksum ='D981A7A41E34C42F86297928A6DBFB634EE79DEAC2C3366E5E96414CAD3CF8E8'
+$url = 'https://github.com/microsoft/perfview/releases/download/P2.0.71/PerfView.exe'
+$checksum ='EAEBCC75C74B57876203204FAFF4E9757A5F834CF84831E602C529F7331FD83B'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyWebFile -PackageName PerfView -FileFullPath "$toolsDir\PerfView.exe" -url $url -checksum $checksum -checksumType 'sha256'
