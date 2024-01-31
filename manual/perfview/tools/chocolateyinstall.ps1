f64ff9bc6b4d19ea563ee2095177855510944fc3
Igor Labutin <igor@labutin.info>
2024-01-31 23:32:10 +0300

    PerfView 3.1.8 release

--- manual/perfview/tools/chocolateyinstall.ps1
+++ manual/perfview/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$name = 'PerfView'
-$url = 'https://github.com/microsoft/perfview/releases/download/v3.1.7/PerfView.exe'
-$checksum ='2E20E6040F14F0C5D5FBCC12E61D287FF4F5816124312755A1D2192DBA5E3E51'
+$url = 'https://github.com/microsoft/perfview/releases/download/v3.1.8/PerfView.exe'
+$checksum ='E8871DFCC72C6D58C6A52ABD7987A742DAC2D6C3CC691D830DEC626169D77598'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyWebFile -PackageName PerfView -FileFullPath "$toolsDir\PerfView.exe" -url $url -checksum $checksum -checksumType 'sha256'
