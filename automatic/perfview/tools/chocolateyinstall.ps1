6e38364da9390407702518f6500cfe994639ec7d
Igor Labutin <igor@labutin.info>
2024-03-09 23:23:46 +0300

    Added scripts for PerfView automatic chocolatey package generation

---
+++ automatic/perfview/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = 'PerfView'
+$url =  'https://github.com/microsoft/perfview/releases/download/v3.1.9/PerfView.exe'
+$checksum ='8f3fa0d8ae425bb50ec1e39eb4e9b7590e54d132250c65f2918b3c49f71f6cf0'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile -PackageName PerfView -FileFullPath "$toolsDir\PerfView.exe" -url $url -checksum $checksum -checksumType 'sha256'
