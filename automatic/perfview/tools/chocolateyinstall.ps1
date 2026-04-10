65d0c5ea287b69be28147cfdf338d8426bb50342
ilabutin <ilabutin@users.noreply.github.com>
2026-04-10 05:25:03 +0000

    update PerfView to version 3.2.2

--- automatic/perfview/tools/chocolateyinstall.ps1
+++ automatic/perfview/tools/chocolateyinstall.ps1
@@ -1,9 +1,10 @@
 $name = 'PerfView'
-$url = 'https://github.com/microsoft/perfview/releases/download/v3.2.1/PerfView.exe'
-$checksum = 'eeee35582c32ad29e17d4d56a5180d79764f0aa3e309a44ec5f06b7cfd5c08c1'
+$url = 'https://github.com/microsoft/perfview/releases/download/v3.2.2/PerfView.exe'
+$checksum = '96579d6ee223f66dab20da0535765f477cd020f6516e0448ec85e2f4a1a80eff'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 Get-ChocolateyWebFile -PackageName PerfView -FileFullPath "$toolsDir\PerfView.exe" -url $url -checksum $checksum -checksumType 'sha256'
+
 
 
 
